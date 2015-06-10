18bf3691f8b48d10689898d48623662ee0f77fec
ybouret <yann.bouret@gmail.com>
2015-06-10 10:49:53 +0200

    commit of cbridge3

---
+++ src/cbridge3/cbridge3.cpp
@@ -0,0 +1,27 @@
+#include "yocto/sequence/vector.hpp"
+#include "yocto/sequence/many-arrays.hpp"
+#include "yocto/program.hpp"
+#include "yocto/math/types.hpp"
+#include "yocto/code/utils.hpp"
+#include "yocto/math/kernel/tao.hpp"
+#include "yocto/ios/ocstream.hpp"
+#include "yocto/math/fcn/zfind.hpp"
+
+using namespace yocto;
+using namespace math;
+
+#if 0
+static const size_t NVAR = 2;
+static double       ATOL = 1e-5;
+static double       BTOL = 1e-4;
+static double       dY   = 1e-4;
+static double       AMIN = 0.001;
+typedef array<double> array_t;
+#endif
+
+
+YOCTO_PROGRAM_START()
+{
+
+}
+YOCTO_PROGRAM_END()

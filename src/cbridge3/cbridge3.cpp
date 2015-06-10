6dda187adb3ce3a256b713bd90257a3e720a41b8
ybouret <yann.bouret@gmail.com>
2015-06-10 16:53:14 +0200

    changed API

--- src/cbridge3/cbridge3.cpp
+++ src/cbridge3/cbridge3.cpp
@@ -12,7 +12,7 @@
 using namespace yocto;
 using namespace math;
 
-static const size_t   NVAR = 3;
+static const size_t   NVAR = 4;
 typedef array<double> array_t;
 typedef ode::Field<double>::Equation equation;
 typedef ode::Field<double>::Callback callback;
@@ -36,7 +36,7 @@ public:
 
     Bridge() : odeint(1e-6),
     R(100),
-    kappa(3),
+    kappa(1.0/R),
     V(NVAR,0)
     {
         odeint.start(NVAR);
@@ -49,24 +49,32 @@ public:
 
     void Equation( array_t &dVds, const double s, const array_t &V)
     {
-        const double r = V[1];
-        const double z = V[2];
-        const double u = V[3];
+        const double r    = V[1];
+        const double z    = V[2];
+        const double drds = V[3];
+        const double dzds = V[4];
 
-        const double cu = Cos(u);
-        const double su = Sin(u);
+        dVds[1] = drds;
+        dVds[2] = dzds;
 
-        dVds[1] = cu;
-        dVds[2] = su;
+        const double f = kappa*kappa * z + dzds/r;
+        dVds[3] =  dzds * f;
+        dVds[4] = -drds * f;
 
-        dVds[3] = -(su/r - 2.0 * kappa * kappa * z);
+    }
 
-
+    void Legalize( array_t &V, double)
+    {
+        const double drds = V[3];
+        const double dzds = V[4];
+        const double speed = Hypotenuse(drds,dzds);
+        V[3] /= speed;
+        V[4] /= speed;
     }
 
     inline void OutputBridge() const
     {
-        const size_t  NB = 100;
+        const size_t  NB = 1024;
         ios::ocstream fp("bridge.dat",false);
         for(size_t i=0;i<=NB;++i)
         {
@@ -75,36 +83,38 @@ public:
         }
     }
 
-    void Build( const double alpha, const double theta )
+    void Build( const double theta, const double alpha )
     {
-        std::cerr << "alpha=" << alpha << std::endl;
         std::cerr << "theta=" << theta << std::endl;
+        std::cerr << "alpha=" << alpha << std::endl;
 
         const double aa = Deg2Rad(alpha);
         const double u0 = Deg2Rad((alpha+theta)-180.0);
         const double r0 = R * Sin(aa);
-        const double z0 = R * (1.0-Cos(aa));
+        const double z0 = R * (1.0-Cos(aa)); // + h
 
         V[1] = r0;
         V[2] = z0;
-        V[3] = u0;
+        V[3] = Cos(u0); // drds
+        V[4] = Sin(u0); // dzds
 
         std::cerr << "dr=" << cos(u0) << std::endl;
         std::cerr << "dz=" << sin(u0) << std::endl;
 
         equation eq(this, & Bridge::Equation);
-
-        double ds = 1e-3;
+        callback cb(this, & Bridge::Legalize);
+        
+        double ds = 1e-2;
         double h  = 1e-8;
         ios::ocstream fp("profile.dat",false);
-        fp("%g %g %g\n",V[1],V[2],V[3]);
+        fp("%g %g\n",V[1],V[2]);
         for(size_t i=0;;++i)
         {
             const double s0 = i*ds;
             const double s1 = (i+1)*ds;
-            odeint(eq,V,s0,s1,h,NULL);
-            fp("%g %g %g\n",V[1],V[2],V[3]);
-            if(s1>=R)
+            odeint(eq,V,s0,s1,h,&cb);
+            fp("%g %g\n",V[1],V[2]);
+            if(s1>=R*2)
                 break;
         }
 
@@ -124,12 +134,12 @@ YOCTO_PROGRAM_START()
 {
     if(argc<=2)
     {
-        throw exception("need alpha theta");
+        throw exception("need theta alpha");
     }
-    const double alpha = strconv::to<double>(argv[1],"alpha");
-    const double theta = strconv::to<double>(argv[2],"theta");
+    const double theta = strconv::to<double>(argv[1],"theta");
+    const double alpha = strconv::to<double>(argv[2],"alpha");
     Bridge B;
-    B.Build(alpha,theta);
+    B.Build(theta,alpha);
     B.OutputBridge();
 }
 YOCTO_PROGRAM_END()

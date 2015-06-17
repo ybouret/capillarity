b6f64abd49d1ca4b0946f54dae6ef6564abb2c6d
ybouret <yann.bouret@gmail.com>
2015-06-17 10:44:34 +0200

    ok for finding average radius

--- src/fit_lens/fit_lens.cpp
+++ src/fit_lens/fit_lens.cpp
@@ -4,6 +4,7 @@
 #include "yocto/ios/icstream.hpp"
 #include "yocto/ios/ocstream.hpp"
 #include "yocto/math/types.hpp"
+#include "yocto/math/opt/cgrad.hpp"
 
 using namespace yocto;
 using namespace math;
@@ -35,24 +36,74 @@ public:
 
     void BuildWith(const double xc,const double R)
     {
-        assert(R>0);
+        //assert(R>0);
         for(size_t i=1;i<=N;++i)
         {
             const double dx = X[i]-xc;
             const double dy = R - Y[i];
             rho[i]   = Hypotenuse(dx, dy);
             alpha[i] =  2 * atan(dx/(dy+rho[i]));
+#if 0
             ios::ocstream fp( vformat("polar%g.dat",R),false);
             for(size_t i=1;i<=N;++i)
             {
                 const double a = alpha[i];
                 const double s = sin(a);
                 const double c = cos(a);
-                const double x = rho[i] * s;
+                const double x = xc+rho[i] * s;
                 const double y = R - rho[i] * c;
                 fp("%g %g %g %g\n",x,y,a,rho[i]);
             }
+#endif
         }
+    }
+
+    void SavePolar(const double xc, const double R) const
+    {
+        ios::ocstream fp( "polar.dat", false);
+        for(size_t i=1;i<=N;++i)
+        {
+            const double a = alpha[i];
+            const double s = sin(a);
+            const double c = cos(a);
+            const double x = xc+ rho[i] * s;
+            const double y = R - rho[i] * c;
+            fp("%g %g %g %g\n",x,y,a,rho[i]);
+        }
+
+    }
+
+    inline double Energy()
+    {
+        double res = 0;
+        double ave = 0;
+        for(size_t i=1;i<=N;++i)
+        {
+            ave += rho[i];
+            //res += Square(rho[i]);
+        }
+        ave/=N;
+        for(size_t i=1;i<=N;++i)
+        {
+            res += Square(rho[i]-ave);
+        }
+        return sqrt(res);
+    }
+
+
+    double H( const array<double> &q)
+    {
+        const double xc = q[1];
+        const double R  = q[2];
+        BuildWith(xc, R);
+        return Energy();
+    }
+
+
+    bool CB(const array<double> &q)
+    {
+        std::cerr << "q=" << q << std::endl;
+        return true;
     }
 
     virtual ~Lens() throw()
@@ -70,8 +121,26 @@ YOCTO_PROGRAM_START()
     {
         const string filename = argv[i];
         Lens lens(filename);
-        lens.BuildWith(0,1);
-        lens.BuildWith(0,2);
+        const size_t nvar = 2;
+        vector<double> q(nvar,0);
+        q[1] = 0;
+        q[2] = 1;
+        vector<double> dq(nvar,1e-4);
+
+        //lens.BuildWith(0,1);
+        //lens.BuildWith(0,2);
+        numeric<double>::scalar_field F(  &lens, & Lens::H );
+        cgrad<double>::callback       cb( &lens, & Lens::CB);
+        cgrad<double> CG;
+
+        if(CG.run(F,q,dq,1e-4,&cb))
+        {
+            std::cerr << "SUCCESS" << std::endl;
+            lens.SavePolar(0, q[1]);
+        }
+
+
+
     }
 }
 YOCTO_PROGRAM_END()
--- src/fit_lens/sim_lens.cpp
+++ src/fit_lens/sim_lens.cpp
@@ -9,15 +9,17 @@ using namespace yocto;
 YOCTO_PROGRAM_START()
 {
     alea_init();
-    const size_t N = 50 + alea_lt(100);
+    const size_t N    = 50 + alea_lt(100);
     const double Xmin = -1;
-    const double Xmax = 1;
+    const double Xmax =  1;
     ios::ocstream fp("lens.dat",false);
+    const double xc = 0.037;
     for(size_t i=0;i<=N;++i)
     {
-        const double x = Xmin + (i * (Xmax-Xmin) )/N;// + (alea<double>()-0.5) * 0.1;
+        const double x =  Xmin + (i * (Xmax-Xmin) )/N;// + (alea<double>()-0.5) * 0.1;
         const double y = 0.8*((1.0 - cos(1.4*x) + (alea<double>()-0.5) * 0.02));
-        fp("%g %g\n",x,y);
+        //const double y = -sqrt(9-x*x);
+        fp("%g %g\n",x+xc,y);
     }
 }
 YOCTO_PROGRAM_END()

6272738c68edffe1abac932c60457ae0b08cfa37
ybouret <yann.bouret@gmail.com>
2015-06-23 09:52:16 +0200

    v7 WIP

--- src/cbridge7/bridge.cpp
+++ src/cbridge7/bridge.cpp
@@ -61,12 +61,20 @@ void Bridge:: RK4(const double z_ini,const double z_end) throw()
 bool Bridge:: FinalRadius(const double height, const double theta_deg, const double alpha_deg, const bool save)
 {
     static vfs & fs = local_fs::instance();
+
+    //__________________________________________________________________________
+    //
+    // do we save ?
+    //__________________________________________________________________________
     if(save)
     {
         fs.try_remove_file("profile.dat");
     }
 
+    //__________________________________________________________________________
+    //
     // check possibility
+    //__________________________________________________________________________
     const double theta = Deg2Rad(theta_deg);
     const double alpha = Deg2Rad(alpha_deg);
     const double omega = lens->omega(alpha);
@@ -76,8 +84,10 @@ bool Bridge:: FinalRadius(const double height, const double theta_deg, const dou
         return false;
     }
 
-
+    //__________________________________________________________________________
+    //
     // prepare initial conditions
+    //__________________________________________________________________________
     const double Rc   = lens->radius(0);
     const double R0   = lens->radius(alpha);
     const double z0   = height + Rc - R0 * Cos(alpha);
@@ -86,6 +96,10 @@ bool Bridge:: FinalRadius(const double height, const double theta_deg, const dou
     Y[1] = r0;
     Y[2] = drdz;
 
+    //__________________________________________________________________________
+    //
+    // do we save ?
+    //__________________________________________________________________________
     auto_ptr<ios::ostream> fp;
     if(save)
     {
@@ -93,17 +107,71 @@ bool Bridge:: FinalRadius(const double height, const double theta_deg, const dou
         (*fp)("%g %g\n", r0, z0 );
     }
 
+    //__________________________________________________________________________
+    //
+    // Try to integrate
+    //__________________________________________________________________________
     double reg[3] = { Y[1],0,0 }; // curvature detector
     size_t num    = 1;
+    bool success  = true;
     for(size_t i=NUM_STEPS;i>0;--i)
     {
         const double z_ini = (i*z0)/NUM_STEPS;
         const double z     = ((i-1)*z0)/NUM_STEPS;
         RK4(z_ini, z);
 
+        //______________________________________________________________________
+        //
         // analyze
+        //______________________________________________________________________
         const double r = Y[0];
-        
+
+        //______________________________________________________________________
+        //
+        // absolute position
+        //______________________________________________________________________
+        if( isnan(r) || isinf(r) || r <= 0)
+        {
+            //std::cerr << "invalid u=" << u << std::endl;
+            success = false;
+            break;
+        }
+
+        //______________________________________________________________________
+        //
+        // Relative position
+        //______________________________________________________________________
+        const double dr = r;
+        const double dz = Rc-z;
+        const double dist = Hypotenuse(dr, dz);
+        const double aa   = 2 * Atan( dr / (dz+dist));
+        const double ra   = lens->radius(aa);
+        if(dist<ra)
+        {
+            success = false;
+            break;
+        }
+
+        //__________________________________________________________________
+        //
+        // test valid curvature
+        //__________________________________________________________________
+        if(num<3)
+        {
+            reg[num++] = r;
+        }
+        else
+        {
+            reg[0] = reg[1];
+            reg[1] = reg[2];
+            reg[2] = r;
+            if((reg[1]+reg[1])>= (reg[0]+reg[2]) )
+            {
+                success = false;
+                break;
+            }
+        }
+
 
         if(save && (r<=4*Rc))
         {
@@ -113,5 +181,5 @@ bool Bridge:: FinalRadius(const double height, const double theta_deg, const dou
 
 
 
-    return false;
+    return success;
 }

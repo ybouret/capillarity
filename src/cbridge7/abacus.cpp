83a16a2a2d04353a8eb33ccb30879320210a543b
ybouret <yann.bouret@gmail.com>
2015-06-24 11:12:22 +0200

    ok for abacus

--- src/cbridge7/abacus.cpp
+++ src/cbridge7/abacus.cpp
@@ -22,8 +22,11 @@ public:
     double Eval(double h, const array_t &a)
     {
         assert(a.size()>0);
-        const double v = S0 - a[1] * h;
-
+        double v = S0 - a[1] * h;
+        if(a.size()>1)
+        {
+            v -= a[2] * h*h;
+        }
         return v;
     }
 
@@ -35,19 +38,23 @@ YOCTO_PROGRAM_START()
         throw exception("need radius clength");
     const double R = strconv::to<double>(argv[1],"R");
     const double C = strconv::to<double>(argv[2],"C");
-    
+
     Lens::Pointer lens( new SphericalLens(R));
     Bridge        bridge(lens, C);
 
     const string a_name = vformat("abacus_R%g_C%g.dat",R,C);
     const string h_name = vformat("height_R%g_C%g.dat",R,C);
+    const string f_name = vformat("curves_R%g_C%g.dat",R,C);
+    const string s_name = vformat("slopes_R%g_C%g.dat",R,C);
     ios::ocstream::overwrite(a_name);
     ios::ocstream::overwrite(h_name);
+    ios::ocstream::overwrite(f_name);
+    ios::ocstream::overwrite(s_name);
 
     bool skip = false;
 
     static const char   wheel[] = "-\\|/";
-    static const size_t nwheel  = sizeof(wheel)/sizeof(wheel[0]);
+    static const size_t nwheel  = sizeof(wheel)/sizeof(wheel[0])-1;
     size_t              iwheel  = 0;
 
     for(int theta = 30; theta < 180; theta += 10 )
@@ -66,8 +73,14 @@ YOCTO_PROGRAM_START()
         const size_t NH = 2+size_t(Ceil(hmax/HRES));
         if(skip)
         {
-            ios::acstream fp(a_name);
-            fp << "\n";
+            {
+                ios::acstream fp(a_name);
+                fp << "\n";
+            }
+            {
+                ios::acstream fp(f_name);
+                fp << "\n";
+            }
         }
         else
         {
@@ -92,8 +105,10 @@ YOCTO_PROGRAM_START()
             const double alpha_deg  = bridge.FindAlpha(height, theta);
             const double alpha      = Deg2Rad(alpha_deg);
             const double S          = lens->surface(alpha);
-            ios::acstream fp(a_name);
-            fp("%g %g\n", height, S);
+            {
+                ios::acstream fp(a_name);
+                fp("%g %g\n", height, S);
+            }
             (std::cerr << "\t[" << wheel[++iwheel%nwheel] << "]  \r").flush();
             if(i<NF)
             {
@@ -102,6 +117,10 @@ YOCTO_PROGRAM_START()
                 fit_v.push_back(0);
             }
         }
+        assert(NF==fit_h.size());
+        assert(NF==fit_s.size());
+        assert(NF==fit_v.size());
+
         std::cerr << std::endl;
         LeastSquares<double>::Samples samples;
         samples.append(fit_h,fit_s,fit_v);
@@ -116,6 +135,8 @@ YOCTO_PROGRAM_START()
         LeastSquares<double>::Function FF( &slope, & Slope::Eval );
         LeastSquares<double>           Fit;
 
+        aorg[1] = (fit_s[1]-fit_s[NF])/(fit_h[NF]-fit_h[1]);
+
         if(!Fit(samples, FF, aorg, used, aerr, 0))
         {
             throw exception("unable to fit...");
@@ -123,9 +144,33 @@ YOCTO_PROGRAM_START()
 
         std::cerr << "aorg=" << aorg << std::endl;
         std::cerr << "aerr=" << aerr << std::endl;
+        std::cerr << "R" << nf << "=" << samples.corr() << std::endl;
+        std::cerr << slope.S0 << "-(" << aorg[1] << ")*x";
+        if(nf>1) {
+            std::cerr << "-(" << aorg[2] << ")*x**2";
+        }
+        std::cerr << std::endl;
+        {
+            ios::acstream fp(f_name);
+            fp("#h  Fit(theta=%d,%g-(%g)*x-(%g)x**2)\n", theta, slope.S0, aorg[1], (nf>1?aorg[2]:0.0) );
+            for(size_t i=1;i<=NF;++i)
+            {
+                fp("%g %g\n", fit_h[i], fit_v[i]);
+            }
+        }
 
+        {
+            ios::acstream fp(s_name);
+            fp("%d %g %g",theta,slope.S0,aorg[1]);
+            if(nf>1)
+            {
+                fp(" %g", aorg[2]);
+            }
+            fp << "\n";
+        }
+        
     }
-
-
+    
+    
 }
 YOCTO_PROGRAM_END()

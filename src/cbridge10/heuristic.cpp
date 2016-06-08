fa9928b60895695b948ef0a5ecbbfa7f7fe6f294
ybouret <yann.bouret@gmail.com>
2016-06-08 16:43:16 +0200

    fitting with slopes only

--- src/cbridge10/heuristic.cpp
+++ src/cbridge10/heuristic.cpp
@@ -11,23 +11,23 @@ YOCTO_PROGRAM_START()
              0.1,    // max angular turn in degrees
              1.0/100 // max speed rate in degrees
              );
-    
+
     int    iarg = 0;
-    
-    
+
+
     if(argc>++iarg)
     {
         B.mu   = strconv::to<double>(argv[iarg],"mu");
     }
     std::cerr << "mu=" << B.mu << std::endl;
-    
-    
-    
+
+
+
     //__________________________________________________________________________
     //
     // find zeta_max
     //__________________________________________________________________________
-    const size_t N=101;
+    const size_t N=21;
     vector<double> zeta(N);
     vector<double> surf(N);
     vector<double> sfit(N);
@@ -35,24 +35,24 @@ YOCTO_PROGRAM_START()
     GLS<double>::Function      poly = _GLS::Create<double,_GLS::Polynomial>();
     GLS<double>::Samples       samples(1);
     (void) samples.append(zeta, surf, sfit);
-    
-    const size_t   m=3;
+
+    const size_t   m=2;
     vector<double> aorg(m);
     vector<double> aerr(m);
     vector<bool>   used(m,true);
     samples.prepare(m);
 
-    
+
     const string heur_name = vformat("heur%g.dat",B.mu);
     ios::ocstream::overwrite(heur_name);
-    
+
     const string sfit_name = vformat("sfit%g.dat",B.mu);
     ios::ocstream::overwrite(sfit_name);
 
     const string coef_name = vformat("coef%g.dat",B.mu);
     ios::ocstream::overwrite(coef_name);
 
-    
+
     for(int t_deg = 40; t_deg <= 170; t_deg+=10)
     {
         std::cerr << "theta=" << t_deg << std::endl; std::cerr.flush();
@@ -60,7 +60,9 @@ YOCTO_PROGRAM_START()
         const double zeta_max  = B.compute_zeta_max( theta);
         std::cerr << "\tzeta_max=" << zeta_max << std::endl;
         const double zeta_half = zeta_max/2;
-        const double zeta_min  = -0.1;
+        //const double zeta_min  = -0.1;
+        const double zeta_min = -zeta_half;
+
         for(size_t i=1;i<=N;++i)
         {
             const double ztmp = zeta_min + (i-1)*(zeta_half-zeta_min)/double(N-1);
@@ -75,13 +77,16 @@ YOCTO_PROGRAM_START()
             ios::acstream hp(heur_name);
             hp << "\n";
         }
-        
+
+
+        used[1] = false;
+        aorg[1] = numeric<double>::pi * Square( sin(B.find_alpha(theta,0.0)) );
 
         if(samples.fit_with(poly, aorg, used, aerr))
         {
             GLS<double>::display(std::cerr,aorg, aerr);
         }
-        
+
         {
             ios::acstream fp(sfit_name);
             for(size_t i=0;i<=N;++i)
@@ -92,7 +97,7 @@ YOCTO_PROGRAM_START()
             }
             fp << "\n";
         }
-        
+
         {
             ios::acstream fp(coef_name);
             fp("%.15g",double(t_deg));
@@ -102,9 +107,9 @@ YOCTO_PROGRAM_START()
             }
             fp << "\n";
         }
-        
-        
-       
+
+
+
 
     }
     

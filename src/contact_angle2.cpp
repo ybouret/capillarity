947befd5f5f53d199400ee428fb5b1877fdacaf5
ybouret <yann.bouret@gmail.com>
2016-06-27 08:54:28 +0200

    need to process picture...

--- src/contact_angle2.cpp
+++ src/contact_angle2.cpp
@@ -20,6 +20,38 @@ using namespace graphics;
 
 YOCTO_PROGRAM_START()
 {
+    // image formats
+    image               &IMG = image::instance();
+    const image::format &PNG = IMG.declare( new png_format()  );
+    IMG.declare( new jpeg_format() );
+    IMG.declare( new tiff_format() );
+
+    // parallel
+    threading::engine server(false);
+    xpatches          xps;
+
+    if(argc<=1)
+    {
+        throw exception("usage: %s filename", program);
+    }
+
+    const string filename = argv[1];
+
+
+    pixmapf           pxm( IMG.loadf(filename,NULL) );
+    PNG.save("image.png", pxm, NULL);
+    xpatch::create(xps, pxm, &server);
+
+    const unit_t w = pxm.w;
+    const unit_t h = pxm.h;
+
+
+    pixmapf grd(w,h);
+    {
+        pixmapf tmp(w,h);
+        compute_gradient(grd, tmp, pxm, xps, &server);
+    }
+    PNG.save("image_grad.png", grd, NULL);
 
 
 }

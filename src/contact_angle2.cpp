970b6e192a66f8b89fff2b3b8bc25ba44cb398ee
ybouret <yann.bouret@gmail.com>
2016-07-26 15:29:54 +0200

    ok, found fit...

--- src/contact_angle2.cpp
+++ src/contact_angle2.cpp
@@ -5,7 +5,6 @@
 #include "yocto/gfx/ops/edges.hpp"
 #include "yocto/gfx/ops/particles.hpp"
 #include "yocto/gfx/ops/histogram.hpp"
-//#include "yocto/gfx/ops/blur.hpp"
 #include "yocto/gfx/ops/filter.hpp"
 
 #include "yocto/ios/ocstream.hpp"
@@ -16,8 +15,11 @@
 #include "yocto/math/stat/descr.hpp"
 #include "yocto/string/conv.hpp"
 
+#include "yocto/math/alg/shapes2d.hpp"
+
 using namespace yocto;
 using namespace gfx;
+using namespace math;
 
 static unit_t y_limit = 0;
 static inline bool is_bad_vertex(const vertex &v) throw()
@@ -25,7 +27,22 @@ static inline bool is_bad_vertex(const vertex &v) throw()
     return v.y>=y_limit;
 }
 
+static inline void load_fit( FitShape2D<double> &fit_shape, const vlist &L, const pixmap<float> &wksp)
+{
+    for(const vnode *node=L.head;node;node=node->next)
+    {
+        const double x = node->vtx.x;
+        const double y = node->vtx.y;
+        const double w = wksp[node->vtx];
+        fit_shape.append(x,y,w);
+    }
+}
 
+static inline RGB Float2RGB(const float f)
+{
+    const uint8_t u = gist::float2byte(f);
+    return RGB(u,u,u);
+}
 
 YOCTO_PROGRAM_START()
 {
@@ -114,6 +131,48 @@ YOCTO_PROGRAM_START()
         std::cerr << "p1.size=" << p1.size << std::endl;
         std::cerr << "p2.size=" << p2.size << std::endl;
 
+        FitConic<double> fit_conic;
+        fit_conic.data.free();
+        fit_conic.data.ensure(p1.size+p2.size);
+
+        load_fit(fit_conic,p1,wksp);
+        load_fit(fit_conic,p2,wksp);
+
+        vector<double> A(6);
+        fit_conic.compute(FitConicEllipse,A);
+        std::cerr << "A=" << A << std::endl;
+
+        point2d<double> center, radius;
+        matrix<double>  rotate(2);
+        fit_conic.Reduce(center, radius, rotate, A);
+        std::cerr << "center=" << center << std::endl;
+        std::cerr << "radius=" << radius << std::endl;
+        std::cerr << "rotate=" << rotate << std::endl;
+
+        pixmap3 wfit(w,h);
+        p1.transfer(wfit,Float2RGB,original);
+        p2.transfer(wfit,Float2RGB,original);
+
+        const double vc  = FitConic<double>::Eval(A,center.x,center.y);
+        RGB          col = named_color::fetch(YGFX_RED);
+        for(unit_t j=0;j<h;++j)
+        {
+            for(unit_t i=0;i<w;++i)
+            {
+                const double val = FitConic<double>::Eval(A,i,j);
+                if(val*vc>=0)
+                {
+                    wfit[j][i] = pixel<RGB>::blend(wfit[j][i], col, 80);
+                }
+            }
+        }
+
+        IMG.save("wfit.png",wfit,NULL);
+
+
+
+
+#if 0
         p1.mask(wksp, 1.0f, 255);
         p2.mask(wksp, 1.0f, 255);
         IMG.save("wsym.png",wksp,NULL);
@@ -125,7 +184,8 @@ YOCTO_PROGRAM_START()
         p2.mask_border(outl, 1.0f, 255);
 
         IMG.save("outline.png",outl,NULL);
-
+#endif
+        
 
     }
 

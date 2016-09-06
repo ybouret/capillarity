445bd0fb184a1f2f14d7a2266acc1ae34347dd2e
ybouret <yann.bouret@gmail.com>
2016-09-06 14:36:02 +0200

    almost ready, need to reduce noise maybe...

--- src/contact_angle_v4.cpp
+++ src/contact_angle_v4.cpp
@@ -13,6 +13,13 @@ using namespace yocto;
 using namespace gfx;
 using namespace math;
 
+
+static unit_t y_limit = 0;
+static inline bool is_bad_vertex(const vertex &v) throw()
+{
+    return v.y>=y_limit;
+}
+
 YOCTO_PROGRAM_START()
 {
     YOCTO_GFX_DECL_FORMAT(jpeg);
@@ -30,12 +37,21 @@ YOCTO_PROGRAM_START()
         const unit_t h = source.h;
         std::cerr << "-- Prepare Engine" << std::endl;
         xpatches xps(source,true);
+        y_limit = h/2;
 
+        //______________________________________________________________________
+        //
         // hard copy
+        //______________________________________________________________________
         const pixmapf img0(source,RGB::to_float,source);
         IMG.save("img0.png",img0,0);
 
         pixmapf       img(img0);
+
+        //______________________________________________________________________
+        //
+        // Filtering if wanted
+        //______________________________________________________________________
         if(argc>2)
         {
             sig = strconv::to_float(argv[2],"sig");
@@ -55,31 +71,84 @@ YOCTO_PROGRAM_START()
         IMG.save("img.png",img,0);
 
 
+        //______________________________________________________________________
+        //
         // detect edges
+        //______________________________________________________________________
         std::cerr << "-- Detecting Edges..." << std::endl;
-        EdgeDetector     ED(w,h);
+        EdgeDetector      ED(w,h);
+        tagmap           &tags  = ED.tags;
+        particles        &edges = ED.edges;
         stencil_scharr_x5 Gx;
         stencil_scharr_y5 Gy;
 
         ED.build_from(img,Gx,Gy,xps);
         ED.tags.colors.shift = YGFX_RED;
         IMG.save("img-grad.png", ED, 0 );
-        IMG.save("img-tags.png", ED.tags, ED.tags.colors, 0);
-        std::cerr << "#edges=" << ED.edges.size() << std::endl;
+        IMG.save("img-tags.png", tags, tags.colors, 0);
+        std::cerr << "#edges=" << edges.size() << std::endl;
 
-
+        //______________________________________________________________________
+        //
+        // copy edges over original picture
+        //______________________________________________________________________
         pixmap3 tgt(source);
-        for(size_t i=ED.edges.size();i>0;--i)
+        for(size_t i=edges.size();i>0;--i)
         {
-            const particle &pa = *ED.edges[i];
-            pa.mask(tgt, named_color::fetch(pa.tag+ED.tags.colors.shift), 255);
+            const particle &pa = *edges[i];
+            pa.mask(tgt, named_color::fetch(pa.tag+tags.colors.shift), 255);
         }
         IMG.save("img-edges.png", tgt, 0);
 
+        //______________________________________________________________________
+        //
+        // close shapes
+        //______________________________________________________________________
         std::cerr << "-- Closing..." << std::endl;
         Filter<float> F(w,h);
         F.Close(ED,xps);
         IMG.save("img-close.png", ED, 0);
+
+        //______________________________________________________________________
+        //
+        // detect final blobs
+        //______________________________________________________________________
+        std::cerr << "-- Final Blobs..." << std::endl;
+        tags.build(ED,8);
+        edges.load(tags);
+        IMG.save("img-blobs.png", tags, tags.colors, 0);
+
+
+        //______________________________________________________________________
+        //
+        // Remove upper part
+        //______________________________________________________________________
+        std::cerr << "-- Removing Vertices" << std::endl;
+#if 0
+        for(size_t i=edges.size();i>0;--i)
+        {
+            particle &pa = *edges[i];
+            vlist     stk;
+            while(pa.size)
+            {
+                vnode *node = pa.pop_front();
+                if(is_bad_vertex(node->vtx))
+                {
+                    tags[node->vtx] = 0;
+                    ED[node->vtx]   = 0;
+                    delete node;
+                }
+                else
+                {
+                    stk.push_back(node);
+                }
+            }
+            pa.swap_with(stk);
+        }
+#endif
+        edges.reject_all_vertices_from(tags,is_bad_vertex);
+        std::cerr << "#final_edges=" << edges.size() << std::endl;
+        IMG.save("img-final.png", tags, tags.colors, 0);
 
     }
 

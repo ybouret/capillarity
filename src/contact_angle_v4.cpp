1e606d0b5492260f657b408e499cdf250a400229
ybouret <yann.bouret@gmail.com>
2016-09-07 17:35:26 +0200

    isolated shape, need to cut low part, startring from middle...

--- src/contact_angle_v4.cpp
+++ src/contact_angle_v4.cpp
@@ -28,6 +28,65 @@ int compare_by_decreasing_height( const particle::ptr &lhs, const particle::ptr 
     return __compare(R.y,L.y);
 }
 
+#include "yocto/associative/map.hpp"
+static inline
+int compare_vtx_by_x(const vertex &lhs, const vertex &rhs) throw()
+{
+    return lhs.x-rhs.x;
+}
+
+typedef vector<vertex>       Vertices;
+
+
+static inline
+void analyze_particle(Vertices       &shape,
+                      const particle &P,
+                      const int       side)
+{
+    typedef map<unit_t,Vertices> VtxMap;
+    VtxMap vmap(P.size,as_capacity);
+    for(const vnode *node = P.head;node;node=node->next)
+    {
+        const vertex v = node->vtx;
+        const unit_t y = v.y;
+        Vertices *V = vmap.search(y);
+        if(V)
+        {
+            V->push_back(v);
+        }
+        else
+        {
+            Vertices vtmp(1,as_capacity);
+            vtmp.push_back(v);
+            if(!vmap.insert(y,vtmp))
+            {
+                throw exception("unexpected vmap failure");
+            }
+        }
+    }
+    std::cerr << "vmap.size=" << vmap.size() << " / " << P.size << std::endl;
+    for(VtxMap::iterator i=vmap.begin();i!=vmap.end();++i)
+    {
+        Vertices &V = *i;
+        quicksort(V,compare_vtx_by_x);
+    }
+    shape.free();
+    shape.ensure(vmap.size());
+    for(VtxMap::iterator i=vmap.begin();i!=vmap.end();++i)
+    {
+        const Vertices &V = *i;
+        if(side<0)
+        {
+            shape.push_back( V.front() );
+        }
+        else
+        {
+            shape.push_back( V.back()  );
+        }
+    }
+}
+
+
 YOCTO_PROGRAM_START()
 {
     YOCTO_GFX_DECL_FORMAT(jpeg);
@@ -144,10 +203,33 @@ YOCTO_PROGRAM_START()
         quicksort(edges, compare_by_decreasing_height);
         particle::ptr A( edges[1] );
         particle::ptr B( edges[2] );
+        if( A->center().x >= B->center().x )
+        {
+            bswap(A,B);
+        }
+        // A is LEFT part, B is RIGHT part
         tgt.copy(source);
-        A->mask(tgt, named_color::fetch(YGFX_RED),   127);
-        B->mask(tgt, named_color::fetch(YGFX_GREEN), 127);
+        A->mask(tgt, named_color::fetch(YGFX_RED),   255);
+        B->mask(tgt, named_color::fetch(YGFX_GREEN), 255);
         IMG.save("img-wksp.png", tgt, 0 );
+
+        // analyze shapes
+        Vertices A_shape;
+        analyze_particle(A_shape,*A,1);
+
+        Vertices B_shape;
+        analyze_particle(B_shape,*B,-1);
+
+        tgt.copy(source);
+        for(size_t i=1;i<=A_shape.size();++i)
+        {
+            tgt[A_shape[i]] = named_color::fetch(YGFX_RED);
+        }
+        for(size_t i=1;i<=B_shape.size();++i)
+        {
+            tgt[B_shape[i]] = named_color::fetch(YGFX_GREEN);
+        }
+        IMG.save("img-shape.png", tgt, 0 );
 
     }
 

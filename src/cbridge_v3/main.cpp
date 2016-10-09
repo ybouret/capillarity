f771fdf132ad28fb4ce306629a5cfb9c4b8be929
ybouret <yann.bouret@gmail.com>
2016-10-09 11:42:44 +0200

    basic par bridge

--- src/cbridge_v3/main.cpp
+++ src/cbridge_v3/main.cpp
@@ -1,10 +1,12 @@
-#include "cbridge.hpp"
+#include "par-bridge.hpp"
 #include "yocto/program.hpp"
 
 YOCTO_PROGRAM_START()
 {
 #include "main-core.cpp"
-
+    
+    ParBridge Bridges(L);
+    
 }
 YOCTO_PROGRAM_END()
 
--- src/cbridge_v3/par-bridge.cpp
+++ src/cbridge_v3/par-bridge.cpp
@@ -1,1 +1,15 @@
+#include "par-bridge.hpp"
 
+ParBridge:: ~ParBridge() throw()
+{
+    
+}
+
+ParBridge:: ParBridge(lua_State *L) : Crew(true)
+{
+    Crew &self = *this;
+    for(size_t i=0;i<self.size;++i)
+    {
+        self[i].build<Bridge,lua_State *>(L);
+    }
+}

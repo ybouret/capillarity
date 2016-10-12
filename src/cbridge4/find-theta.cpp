16bbb6956324ee6200a818933e18e67c8bfb93e5
ybouret <yann.bouret@gmail.com>
2016-10-12 09:23:06 +0200

    start new find theta...

--- src/cbridge4/find-theta.cpp
+++ src/cbridge4/find-theta.cpp
@@ -5,7 +5,25 @@ YOCTO_PROGRAM_START()
 {
 #include "main-core.cpp"
 
-    
+    const double zeta      = Lua::Config::Get<lua_Number>(L, "zeta" );
+    const double alpha_deg = Lua::Config::Get<lua_Number>(L,"alpha");
+    const double alpha     = Deg2Rad(alpha_deg);
+    std::cerr << "alpha=" << alpha_deg << std::endl;
+
+    B.SaveLens("lens.dat", zeta);
+
+    {
+        ios::wcstream fp("profile.dat");
+        ios::wcstream rp("results.dat");
+
+        for(double theta_deg = 1; theta_deg <= 179; theta_deg += 1)
+        {
+            const double theta = Deg2Rad(theta_deg);
+            const double ans = B.profile(alpha,theta,zeta,&fp,false);
+            fp << "\n";
+            rp("%g %g\n", theta_deg, ans );
+        }
+    }
 
 }
 YOCTO_PROGRAM_END()

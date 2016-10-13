ca6fa0a2eb1da453262dd9ee8e117e7a6c44ca4c
ybouret <yann.bouret@gmail.com>
2016-10-13 15:28:46 +0200

    function is not monotonic...

---
+++ src/cbridge4/theta-of-alpha.cpp
@@ -0,0 +1,30 @@
+#include "bridge.hpp"
+#include "yocto/program.hpp"
+
+YOCTO_PROGRAM_START()
+{
+#include "main-core.cpp"
+
+    double       zeta      = Lua::Config::Get<lua_Number>(L, "zeta" );
+
+    B.SaveLens("lens.dat", zeta);
+
+    ios::wcstream fp("profile.dat");
+    ios::ocstream::overwrite("results.dat");
+    bool isFlat;
+    for(double alpha_deg=0.1;alpha_deg<=90; alpha_deg += 0.1)
+    {
+        const double alpha = Deg2Rad(alpha_deg);
+        const double theta = B.find_theta(alpha,zeta,isFlat);
+        std::cerr << "alpha=" << alpha_deg << ", theta=" << Rad2Deg(theta) << std::endl;
+        if(theta>0)
+        {
+            B.profile(alpha, theta, zeta,&fp);
+            fp << "\n";
+        }
+        ios::ocstream::echo("results.dat","%g %g\n", alpha_deg, Rad2Deg(theta) );
+    }
+
+
+}
+YOCTO_PROGRAM_END()

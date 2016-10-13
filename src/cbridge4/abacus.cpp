fd7cd578a90624058d9ac23078d16a1e11297060
ybouret <yann.bouret@gmail.com>
2016-10-13 20:17:13 +0200

    need to sort good and bad points...

--- src/cbridge4/abacus.cpp
+++ src/cbridge4/abacus.cpp
@@ -1,5 +1,6 @@
 #include "bridge.hpp"
 #include "yocto/program.hpp"
+#include "yocto/sort/quick.hpp"
 
 YOCTO_PROGRAM_START()
 {
@@ -9,19 +10,40 @@ YOCTO_PROGRAM_START()
 
     ios::ocstream::overwrite(filename);
     const double zeta_min = hmin/B.R0;
-    const size_t N        = 20;
+    const size_t N        = 40;
 
     Vector h;
     Vector A;
-    for(double theta_deg=100; theta_deg <= 170; theta_deg += 10 )
+    double alphas[2];
+    bool   isFlat = false;
+    
+    for(double theta_deg=120; theta_deg <= 160; theta_deg += 10 )
     {
         std::cerr << "THETA=" << theta_deg << std::endl;
         std::cerr << "\tlooking for zeta_max..." << std::endl;
         const double theta    = Deg2Rad(theta_deg);
         const double zeta_max = B.find_zeta_max(theta);
         std::cerr << "zeta_max=" << zeta_max << std::endl;
-        ios::ocstream::echo(filename, "%g %g\n", theta_deg, zeta_max);
-
+        const double ampli = zeta_max - zeta_min;
+        for(size_t i=0;i<=N;++i)
+        {
+            double   zeta = zeta_min;
+            if(i>=N) zeta = zeta_max; else zeta += (i*ampli)/double(N);
+            const size_t n = B.find_alpha(theta, zeta, alphas, isFlat);
+            for(size_t j=0;j<n;++j)
+            {
+                const double alpha = alphas[j];
+                h.push_back( zeta * B.R0 );
+                A.push_back( numeric<double>::pi * Square( B.R0 * sin(alpha) ) );
+            }
+        }
+        co_qsort(A,h);
+        ios::acstream fp(filename);
+        for(size_t i=1;i<=A.size();++i)
+        {
+            fp("%g %g\n", h[i], A[i]);
+        }
+        
     }
 
 }

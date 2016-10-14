f0792f6ef626d4ecded5e8f9f6425b2d8199b146
ybouret <yann.bouret@gmail.com>
2016-10-14 09:28:54 +0200

    found solution...

--- src/cbridge4/abacus.cpp
+++ src/cbridge4/abacus.cpp
@@ -6,44 +6,52 @@ YOCTO_PROGRAM_START()
 {
 #include "main-core.cpp"
     const double hmin = Lua::Config::Get<lua_Number>(L, "hmin");
-    const string filename = "abacus4.dat";
+    const string filename  = "abacus4.dat";
+    const string filename2 = "abacus4b.dat";
 
     ios::ocstream::overwrite(filename);
-    const double zeta_min = hmin/B.R0;
-    const size_t N        = 40;
+    ios::ocstream::overwrite(filename2);
 
-    Vector h;
-    Vector A;
+    const double zeta_min = hmin/B.R0;
+    const size_t N        = 100;
+
+    //Vector h;
+    //Vector A;
     double alphas[2];
     bool   isFlat = false;
     
-    for(double theta_deg=120; theta_deg <= 160; theta_deg += 10 )
+    for(double theta_deg=110; theta_deg <= 170; theta_deg += 10 )
     {
         std::cerr << "THETA=" << theta_deg << std::endl;
         std::cerr << "\tlooking for zeta_max..." << std::endl;
         const double theta    = Deg2Rad(theta_deg);
         const double zeta_max = B.find_zeta_max(theta);
-        std::cerr << "zeta_max=" << zeta_max << std::endl;
         const double ampli = zeta_max - zeta_min;
+        std::cerr << "\tzeta_max=" << zeta_max << std::endl;
+        
         for(size_t i=0;i<=N;++i)
         {
             double   zeta = zeta_min;
             if(i>=N) zeta = zeta_max; else zeta += (i*ampli)/double(N);
             const size_t n = B.find_alpha(theta, zeta, alphas, isFlat);
-            for(size_t j=0;j<n;++j)
+            const double h = zeta*B.R0;
+
+            switch(n)
             {
-                const double alpha = alphas[j];
-                h.push_back( zeta * B.R0 );
-                A.push_back( numeric<double>::pi * Square( B.R0 * sin(alpha) ) );
+                case 0: break;
+                case 1: ios::ocstream::echo(filename, "%g %g\n", h, numeric<double>::pi * Square( B.R0 * sin(alphas[0]) )); break;
+                case 2:
+                    ios::ocstream::echo(filename,  "%g %g\n", h, numeric<double>::pi * Square( B.R0 * sin(alphas[1]) ));
+                    if(zeta>0)
+                    {
+                        ios::ocstream::echo(filename2, "%g %g\n", h, numeric<double>::pi * Square( B.R0 * sin(alphas[0]) ));
+                    }
+                    break;
+
             }
         }
-        co_qsort(A,h);
-        ios::acstream fp(filename);
-        for(size_t i=1;i<=A.size();++i)
-        {
-            fp("%g %g\n", h[i], A[i]);
-        }
-        
+        ios::ocstream::echo(filename, "\n");
+        ios::ocstream::echo(filename2,"\n");
     }
 
 }

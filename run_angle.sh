21322ddd5d5ab938d88fc3eb05408aaa710efd9c
ybouret <yann.bouret@gmail.com>
2016-11-28 11:56:50 +0100

    ok, scripted

--- run_angle.sh
+++ run_angle.sh
@@ -1,1 +1,30 @@
+PROG=./bin/contact_angle
+ROOT=data/article/pictures
 
+#N10t_1
+$PROG $ROOT/N10t_1/1 right
+$PROG $ROOT/N10t_1/2 right
+$PROG $ROOT/N10t_1/3 right
+
+#N11t_1
+$PROG $ROOT/N11t_1/1 left
+
+#N11t_2
+$PROG $ROOT/N11t_2/1 left
+$PROG $ROOT/N11t_2/2 left
+$PROG $ROOT/N11t_2/3 left
+
+#N11t_5
+$PROG $ROOT/N11t_5/1 right
+$PROG $ROOT/N11t_5/2 right
+$PROG $ROOT/N11t_5/3 right
+
+#N11t_6
+$PROG $ROOT/N11t_6/1 left
+$PROG $ROOT/N11t_6/2 left
+$PROG $ROOT/N11t_6/3 left
+
+#N13t_1
+$PROG $ROOT/N13t_1/1 left
+$PROG $ROOT/N13t_1/2 left
+$PROG $ROOT/N13t_1/3 left

dc6f820c37cdcbf56b03d5243a2969018a26f53f
ybouret <yann.bouret@gmail.com>
2017-07-25 12:30:08 +0200

    WIP gather angles

--- src/gather_angles.cpp
+++ src/gather_angles.cpp
@@ -1,16 +1,33 @@
 #include "yocto/program.hpp"
 #include "yocto/fs/local-fs.hpp"
+#include "yocto/ptr/auto.hpp"
 
 using namespace yocto;
 
 YOCTO_PROGRAM_START()
 {
 
+    vfs &fs = local_fs::instance();
+
     for(int iarg = 1; iarg < argc; ++iarg )
     {
         string dirname = argv[iarg];
         (void)vfs::as_directory(dirname);
         std::cerr << "processing " << dirname << std::endl;
+        auto_ptr<vfs::scanner> scan( fs.new_scanner( dirname ) );
+        for( const vfs::entry *ep = scan->next(); ep ; ep = scan->next() )
+        {
+            //std::cerr << '\t' << '<' << ep->attr << '>' << ep->base_name << " [" << ep->path << "]" << std::endl;
+            if( ep->is_regular() )
+            {
+                const string ext = ep->extension;
+                if( "txt" == ext )
+                {
+                    std::cerr << "\t[" << ep->path << "]" << std::endl;
+                }
+            }
+        }
+
     }
 
 }

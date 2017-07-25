53b601841437b72435aed774d4587f4aff60ed3b
ybouret <yann.bouret@gmail.com>
2017-07-25 11:53:22 +0200

    will work on gather

--- src/gather_angles.cpp
+++ src/gather_angles.cpp
@@ -6,5 +6,12 @@ using namespace yocto;
 YOCTO_PROGRAM_START()
 {
 
+    for(int iarg = 1; iarg < argc; ++iarg )
+    {
+        string dirname = argv[iarg];
+        (void)vfs::as_directory(dirname);
+        std::cerr << "processing " << dirname << std::endl;
+    }
+
 }
 YOCTO_PROGRAM_END()

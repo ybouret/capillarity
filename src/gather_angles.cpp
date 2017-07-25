8990958e1ed98bbc16f311014cea52c257327d05
ybouret <yann.bouret@gmail.com>
2017-07-25 12:59:51 +0200

    almost done

--- src/gather_angles.cpp
+++ src/gather_angles.cpp
@@ -1,6 +1,9 @@
 #include "yocto/program.hpp"
 #include "yocto/fs/local-fs.hpp"
 #include "yocto/ptr/auto.hpp"
+#include "yocto/sequence/vector.hpp"
+#include "yocto/ios/icstream.hpp"
+#include "yocto/ios/ocstream.hpp"
 
 using namespace yocto;
 
@@ -15,6 +18,9 @@ YOCTO_PROGRAM_START()
         (void)vfs::as_directory(dirname);
         std::cerr << "processing " << dirname << std::endl;
         auto_ptr<vfs::scanner> scan( fs.new_scanner( dirname ) );
+        vector<string> lines;
+
+        // load content and format lines
         for( const vfs::entry *ep = scan->next(); ep ; ep = scan->next() )
         {
             //std::cerr << '\t' << '<' << ep->attr << '>' << ep->base_name << " [" << ep->path << "]" << std::endl;
@@ -26,7 +32,29 @@ YOCTO_PROGRAM_START()
                     string id = ep->base_name;
                     id.trim(10);
                     std::cerr << "\t[" << ep->path << "] -> " << id << std::endl;
+                    string line;
+                    ios::icstream fp(ep->path);
+                    if( !fp.gets(line) )
+                    {
+                        throw exception("couldn't get line from '%s'", id.c_str() );
+                    }
+                    std::cerr << "\t\t" << line << std::endl;
+                    line += ' ';
+                    line += id;
+                    lines.push_back(line);
                 }
+            }
+        }
+
+
+        // prepare output
+        const string output = dirname + "theta.txt";
+        std::cerr << "saving into " << output << std::endl;
+        {
+            ios::wcstream fp(output);
+            for(size_t i=1;i<=lines.size();++i)
+            {
+                fp << lines[i] << '\n';
             }
         }
 

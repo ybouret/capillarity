0c5041b036b0d33e3534b064ed0d86f342cdc476
ybouret <yann.bouret@gmail.com>
2017-08-04 13:00:52 +0200

    ok, one thing done...

--- src/cbridge4/main.cpp
+++ src/cbridge4/main.cpp
@@ -45,10 +45,10 @@ YOCTO_PROGRAM_START()
         {
             ios::wcstream fp(output);
 
-            fp("#h A alpha theta h_corr t\n");
+            fp("#h A alpha theta h_corr t\r\n");
             for(size_t i=1;i<=app.h.size();++i)
             {
-                fp("%g %g %g %g %g %g\n", app.h[i], app.A[i], Rad2Deg(app.alpha[i]), Rad2Deg(app.theta[i]), app.h_corr[i], app.t[i]);
+                fp("%g %g %g %g %g %g\r\n", app.h[i], app.A[i], Rad2Deg(app.alpha[i]), Rad2Deg(app.theta[i]), app.h_corr[i], app.t[i]);
             }
         }
 

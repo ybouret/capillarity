f0dc50d5ece3a4d649d35c3404bc425974517c29
ybouret <yann.bouret@gmail.com>
2017-09-29 11:37:24 +0200

    prepare scripting...

---
+++ run_cbridge3.sh
@@ -0,0 +1,8 @@
+find sub_cycles -name "*.dat" | xargs rm
+
+./bin/cbridge4 sub_cycles/N11t1_R82mm_vitesse_deplact5mu_par_sec p1=-5 p2=17 p3=-9
+./bin/cbridge4 sub_cycles/N11t2_R82mm_vitesse_deplact5mu_par_sec p1=-5 p2=17 p3=-9
+./bin/cbridge4 sub_cycles/N11t3_R82mm_vitesse_deplact5mu_par_sec p1=-5 p2=17 p3=-9
+./bin/cbridge4 sub_cycles/N11t4_R82mm_vitesse_deplact5mu_par_sec p1=-5 p2=17 p3=-9
+
+
